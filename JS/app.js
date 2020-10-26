efe3774e23e525d6115b92597013c404c749becd
RoyalFang <GAMihov18@codingburgas.bg>
2020-10-26 21:59:29 +0200

    Add main files and simple error function in app.js

---
+++ JS/app.js
@@ -0,0 +1,26 @@
+console.log("Link");
+
+
+//Functions
+function ThrowError(popUp ,errorCode = 0) {
+  if (popUp) {
+    alert(`Error code: ${errorCode}`);  
+  }
+  else{
+    console.log(`Error code: ${errorCode}`);
+  }
+  /*
+  Error codes:
+  1 - Wrong input
+  2 - Incorrect request
+  3 - 
+  4 - 
+  5 - 
+  6 - 
+  6 - 
+  7 - 
+  8 - 
+  9 - 
+  10 - 
+  */
+}
